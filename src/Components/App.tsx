7b1aca158ac75174ff14202985d9c5f1c29b0b0d
Mikołaj Chojnacki <mikolaj.chojnacki@wp.pl>
2023-08-09 19:03:51 +0200

    Add Styles
    	- Add Reset styles
    	- Add style settings

---
+++ src/Components/App.tsx
@@ -0,0 +1,12 @@
+import '../GlobalStyles/Reset.scss';
+import '../GlobalStyles/Settings.scss';
+
+const App = () => {
+  return (
+    <div className="App">
+      Witam
+    </div>
+  );
+}
+
+export default App;
