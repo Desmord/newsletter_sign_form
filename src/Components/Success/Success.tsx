7f15f5649776cabc5604148d2fae7a727e509515
Mikołaj Chojnacki <mikolaj.chojnacki@wp.pl>
2023-08-10 20:25:45 +0200

    Create Success component

--- src/Components/Success/Success.tsx
+++ src/Components/Success/Success.tsx
@@ -1,9 +1,21 @@
+import Button from '../Common/Button/Button'
+
 import styles from './Success.module.scss'
 
 const Success = ({ setIsSuccess }: { setIsSuccess: Function }) => {
     return (
         <div className={styles.container}>
-            sukces
+            <img src="./assets/icon-success.svg" alt="img"></img>
+            <div className={styles.tittle}>
+                Thanks for subscribing!
+            </div>
+            <div className={styles.description}>
+                A confiramtion email has been sent to email@example.com. Please open it and
+                click the button inside to confirm your subscription.
+            </div>
+            <div className={styles.button}>
+                <Button func={setIsSuccess} text={`Dismiss message`} />
+            </div>
         </div>
     )
 }
